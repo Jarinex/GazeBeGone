66751c27ac7dda0f480dccceb03c754beca3cbfa
Jarinex <31430822+Jarinex@users.noreply.github.com>
2020-11-01 13:08:28 -0700

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,94 @@
+﻿using UnityModManagerNet;
+using System;
+using System.Reflection;
+using System.Linq;
+using Kingmaker.Blueprints;
+using Kingmaker;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.UnitLogic.FactLogic;
+using Kingmaker.UnitLogic.Mechanics.Components;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+using Kingmaker.Designers.Mechanics.Buffs;
+using System.Collections.Generic;
+using Kingmaker.Blueprints.Items;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace GazeBeGone
+{
+    internal class Main
+    {
+    
+        internal static UnityModManagerNet.UnityModManager.ModEntry.ModLogger logger;
+        internal static Harmony.HarmonyInstance harmony;
+        internal static LibraryScriptableObject library;
+
+        static readonly Dictionary<Type, bool> typesPatched = new Dictionary<Type, bool>();
+        static readonly List<String> failedPatches = new List<String>();
+        static readonly List<String> failedLoading = new List<String>();
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        internal static void DebugLog(string msg)
+        {
+            if (logger != null) logger.Log(msg);
+        }
+        internal static void DebugError(Exception ex)
+        {
+            if (logger != null) logger.Log(ex.ToString() + "\n" + ex.StackTrace);
+        }
+        internal static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+                harmony = Harmony.HarmonyInstance.Create(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            }
+            catch (Exception ex)
+            {
+                DebugError(ex);
+                throw ex;
+            }
+            return true;
+        }
+        [Harmony.HarmonyPatch(typeof(LibraryScriptableObject), "LoadDictionary")]
+        [Harmony.HarmonyPatch(typeof(LibraryScriptableObject), "LoadDictionary", new Type[0])]
+        static class LibraryScriptableObject_LoadDictionary_Patch
+        {
+            static void Postfix(LibraryScriptableObject __instance)
+            {
+                var self = __instance;
+                if (Main.library != null) return;
+                Main.library = self;
+                try
+                {
+                    Main.logger.Log("Loading Gaze Be Gone");
+
+
+
+                   
+                    
+                    GazeBeGone.load();
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    Main.DebugError(ex);
+                }
+            }
+        }
+
+        internal static Exception Error(String message)
+        {
+            logger?.Log(message);
+            return new InvalidOperationException(message);
+        }
+    }
+}
+
